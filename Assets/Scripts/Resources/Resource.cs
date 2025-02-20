46572d3ad822fd23655273562fa080f774f2fd19
Irons-A <120574631+Irons-A@users.noreply.github.com>
2025-02-20 14:54:51 +0300

    v9.1

--- Assets/Scripts/Resources/Resource.cs
+++ Assets/Scripts/Resources/Resource.cs
@@ -7,19 +7,11 @@ public class Resource : MonoBehaviour, ITargetable
     public event Action<Resource> IsCollected;
 
     [field: SerializeField] public int Value { get; private set; } = 1;
-
-    //public bool IsAssignedToBot { get; private set; } = false;
     public Vector3 Position => transform.position;
 
     public void ResetParameters()
     {
         transform.SetParent(null);
-        //IsAssignedToBot = false;
-    }
-
-    public void MarkAsAssigned()
-    {
-        //IsAssignedToBot = true;
     }
 
     public void MarkAsCollected()
